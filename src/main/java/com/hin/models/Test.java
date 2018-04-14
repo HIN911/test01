f79223e2781591adfd1dbaae2bbabd879dd7f272
HIN <838794302@qq.com>
2018-04-14 11:11:06 +0800

    first commit

---
+++ src/main/java/com/hin/models/Test.java
@@ -0,0 +1,16 @@
+package com.hin.models;
+
+public class Test {
+
+	
+	private String name;
+	
+	private int age;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
